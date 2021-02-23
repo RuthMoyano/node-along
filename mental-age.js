5341dbffee9a51befa783cad60b4ee34cf4781a3
Ruth Moyano <ruth.moyano@codeimmersives.com>
2021-02-23 14:22:38 -0600

    [Add all changes]

--- mental-age.js
+++ mental-age.js
@@ -1,1 +1,14 @@
 const getInput = require("./get-input.js");
+
+
+const userAge = getInput();
+if (userAge === '--help') {
+    console.log('This function returns your mental age.');
+    console.log('Please enter your age after "node mental-age.js" and a space.');
+}
+console.log(userAge / 2 + 3);
+
+console.log('You are ' + userAge + ' years old.');
+
+console.log('Your mental age is ' + mentalAge + ' years old.');
+
